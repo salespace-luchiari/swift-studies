4e2c8f12ac2968ce35bf07f6bf0c890ee553edfa
Laura Luchiari <laura.luchiari@fyve.com.br>
2024-04-26 10:47:06 -0300

    chore: cria projeto

---
+++ swift-studies/swift_studiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_studiesApp.swift
+//  swift-studies
+//
+//  Created by Laura Ranucci Luchiari on 26/04/24.
+//
+
+import SwiftUI
+
+@main
+struct swift_studiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
